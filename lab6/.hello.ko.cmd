bca532fe9b8a5c62e218f0df35858dbb04283c42
Lux-ex-tenebris <kostyanikovvladik@gmail.com>
2020-05-17 23:46:06 +0300

    lab 6  first commit

---
+++ lab6/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vlad/repos/dir1/DPKS_lab6/lab6/hello.ko := ccache arm-eabi-ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/vlad/repos/dir1/DPKS_lab6/lab6/hello.ko /home/vlad/repos/dir1/DPKS_lab6/lab6/hello.o /home/vlad/repos/dir1/DPKS_lab6/lab6/hello.mod.o ;  true
---
+++ lab6/.tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/vlad/repos/dir1/DPKS_lab6/lab6/hello.ko
+/home/vlad/repos/dir1/DPKS_lab6/lab6/hello.o
+
